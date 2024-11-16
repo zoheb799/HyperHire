f249d59dad1779a14df3f39c144b5f25e310ef60
zoheb799 <zohebuddin25@gmail.com>
2024-11-17 03:51:28 +0530

    updated svgs height and width

--- src/public/svgs/menu.tsx
+++ src/public/svgs/menu.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Menu: React.FC = () => (
   <svg
-    width="64px"
-    height="64px"
+    width="32px"
+    height="32px"
     viewBox="0 0 24 24"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
